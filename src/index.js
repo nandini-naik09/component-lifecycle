819d1485efd05d30469aaac0e5b8cd67de422416
nandini-naik09 <nandinipardeshi99@gmail.com>
2023-06-01 12:57:32 +0530

    component-Unmounting

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Lifecycle extends React.Component {
+  constructor(props) {
+    super(props);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.state = {
+      data: 0,
+    };
+    
+    this.setNewValue = this.setNewValue.bind(this);
+  }
+  setNewValue() {
+    this.setState({ data: this.state.data + 1 });
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.setNewValue}>Keep Adding</button>
+        <Content Number={this.state.data}></Content>
+      </div>
+    );
+  }
+}
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>{this.props.Number}</h3>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Lifecycle />, document.getElementById("root"));
+
+setTimeout(() => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("root"));
+},10000);
